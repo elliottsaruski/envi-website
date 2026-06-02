65341d1330ecf8f550de02c54247fc6ea59cccc7
elliottsaruski <elliottxsaruski@gmail.com>
2026-06-01 22:58:15 -0400

    create cloudinary img util + remove mediaData unused json

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,4 +1,7 @@
 const CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME;
 
 export const audioUrl = (path) =>
-  `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/f_mp3/${path}`;
+  `https://res.cloudinary.com/${CLOUD_NAME}/video/upload/${path}`;
+
+export const imageUrl = (publicId) =>
+  `https://res.cloudinary.com/${CLOUD_NAME}/image/upload/${publicId}`;
